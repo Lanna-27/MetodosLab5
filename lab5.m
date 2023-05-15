30d35f9540ef26215145f0dd143d5c23dc83d336
Lanna-27 <lalacast2000@gmail.com>
2023-05-15 01:14:36 -0500

    modificciones

--- lab5.m
+++ lab5.m
@@ -1,3 +1,7 @@
+clear
+clc
+clf('reset')
+
 %Pide la función incial
 fun = input("Ingresa la función a evaluar en formato simbolico: ");
 %Dar la opción de elegir derivacion o integracion
@@ -9,94 +13,105 @@ if(opc == 1)
     %Derivada del polinomio interpolador de Newton de grado N
     %para aproximar numéricamente f'(t0)
     %Se piden los parametros necesarios
-    x1=input('Ingrese el vector de  abscisas(x) equidistantes: ');
-    f=input('Ingrese el vector de ordenadas(fx): ');
-    while(length(vec_x) ~= length(vec_y))
-        printf("Los vectores ingresados no son de la misma longitud\n")
-        vec_x = input("Vector de X: ");
-        vec_y = input("Vector de Y: ");
-    end
+    t0=input('Ingrese el valor en el que desea evaluar la derivada: ');
+    h=input('Ingrese el valor de h: ');
 
-    a=input('Ingrese un valor para evaluar la derivada: ');
-    % pivote=input('Ingrese la casilla del dato que usara como pivote en la construcción del polinomio: ');
-    % grado=input('¿De que grado quiere que sea el polinomio aproximado? : ');
+    t1 = t0+h;
+    t2 = t0-h; 
 
-    %Se calcula la magnitud del vector de abscisas
-    n=length(x1);
+    % x=[t2, t0, t1]; 
 
-    %Se rectifica que las abscisas sean equidistantes
-    for z=1:n
-        if x1(z+1)-x1(z)~=x1(z+2)-x1(z+1)
-            disp('Los puntos de las abscisas no son equidistantes')
-            x1=input('Ingrese el vector de  abscisas(x) equidistantes: ');
-        else
-            break
-        end
-    end
+    % f = fun(x);
+
+    xderv=[t0, t1, t2];
+
+    f=fun(xderv);
 
     %Se crea la matriz de ceros para crear la tabla de diferencias finitas
-    b=zeros(n);
+    b=zeros(3);
     %Se ponen como primera columna el valor de las ordenadas
     b(:,1)=f';
-    %Se calcula la diferencia entre dos puntos consecutivos de las abscisas
-    h=x1(2)-x1(1);
 
     %Se construye la tabla de diferencias finitas
-    for j=2:n
-    for i=j:n
-    b(i,j)=(b(i,j-1)-b(i-1,j-1));
-    end
+    for j=2:3
+        for i=j:3
+            b(i,j)=(b(i,j-1)-b(i-1,j-1));
+        end
     end
 
     %Calculo del dato interpolado ( productoria)
     fprintf('\nLa matriz de diferencias dividas es : ')
     %Se enumeran las filas en la tabla de resultados
-    num=[0:1:n-1];
-    M=[num' x1' b];
-    array2table(M,'VariableNames',{'Punto','Xi','f(Xi)','Δƒ[xi]','Δƒ²[xi]','Δƒ³[xi]','Δƒ4[xi]','Δƒ5[xi]'})
+    num=[0:1:3-1];
+    M=[num' x' b];
+    array2table(M,'VariableNames',{'Punto','Xi','f(Xi)','Δƒ[xi]','Δƒ²[xi]'})
 
     %Construccion del polinomio interpolador con base en el pivote y
     %la tabla de diferencias
     syms s
     %Se inicializa el primer valor para el polinomio junto a la productoria
-    polsig=M(pivote,3);
+    polsig=M(1,3);
     produ=1;
-    %Se crea el polinomio del grado deseado por el usuario (maximo n-1)
-    for i=1:grado
-    produ=produ*(s-i+1)/factorial(i);
-    polsig=polsig+produ*M(i+pivote,i+3);
+    polderv=bderv(2,2);
+    prodderv = 1;
+    %Se crea el polinomio de grado n-1
+    for i=1:(3-1)
+        %polinomio interpolado
+        produ=produ*(s-i+1)/factorial(i);
+        polsig=polsig+produ*M(i+1,i+3);
+        
+        if i<n-1
+        prodderv=prodderv*(s-i+2)/factorial(i);
+
+        
     end
 
     %Se imprime el polinomio construido
-    polsig;
-    polsig=expand(polsig);
+    polsig
+    polderv
+    polexp=expand(polsig);
     disp('El polinomio de interpolacion de newton es: ')
-    pretty(polsig)
+    pretty(polexp)
+    disp('Y su derivada es: ')
+    polder=expand(polderv);
+    pretty(polder)
     %Se convierte el polinomio construido en una función en linea para evaluar
     %los diferentes valores de s
-    polfinal=inline(polsig);
+    polfinal=inline(polexp);
+    poldervfinal=inline(polder); 
     %Calculo del valor 'a' a interpolar
-    Seval=(a-x1(pivote))/h;
-    fprintf('\nEl valor del punto %.3f es : ',a)
-    polfinal(Seval)
+    Seval=(t0-xderv(1))/h;
+    fprintf('\nEl valor del punto %.3f es : ',t0)
+    poldervfinal(t0)
 
     %Se convierten los valores x1 correspondiente al método de
     %diferencias finitas
-    Sgrafica=(x1-x1(pivote))/h;
+    ev=[-0.5:0.1:1.5];
+    %Se convierten los valores x1 correspondiente al método de
+    %diferencias finitas
+    Sgrafica=(ev-x(1))/h;
+    Sgraficaderv=(ev-xderv(1))/h;
 
+    fun2 = @(x) -sin(x);
     %Grafica de los datos experimentales y el polinomio obtenido
     polevaluado=polfinal(Sgrafica);
-    plot(x1,f,'r')
+    polevalderv=poldervfinal(Sgraficaderv);
+    fplot(fun,[-0.5,1.5],'r')
     grid on
-    axis([x1(1) x1(n) f(1) f(n)])
+    axis([0 1.2 -1 1])
     hold on
-    plot(x1,polevaluado,'b')
+    fplot(fun2,[-0.5,1.5],'r')
     hold on
-    plot(a,polfinal(Seval),'r.','markersize',12)
-    xlabel('T(°F)')
-    ylabel('Pvapor(lb/in²)')
-    title('Diferencias finitas')
-    legend('Datos reales','Interpolación','Location','northwest')
+    plot(ev,polevaluado,'b')
+    hold on
+    plot(ev,polevalderv, "g")
+    hold on
+    plot(t0,polfinal(Seval),'r.','markersize',12)
+    hold on
+    xlabel('Rad')
+    ylabel('F(x)')
+    title('Derivación')
+    legend('Datos reales','Interpolación','Derivada','Location','southwest')
 
 
 %----PUNTOS 2 Y 3 (integración)--------
@@ -111,7 +126,3 @@ elseif(opc == 2)
 
     end
 end
-
-
-#punto 2
-#punto 3
